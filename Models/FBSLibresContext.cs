bcd2953e1bd36e5cec9a4ef8f86ba2c8ff6f499c
cabp1996 <41761406+cabp1996@users.noreply.github.com>
2019-01-05 15:55:27 -0500

    test
    test

--- Models/FBSLibresContext.cs
+++ Models/FBSLibresContext.cs
@@ -24,7 +24,7 @@ namespace BLOGLIBRES.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("server=DESKTOP-NQEJ9JQ\\DBSOFIA;user=sa;password=sofi;database=FBSLibres");
+                optionsBuilder.UseSqlServer("server=ALLINONE10Q;user=sa;password=cesar;database=FBSLibres");
             }
         }
 
